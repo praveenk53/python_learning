2322df8c7262da8228959d0fc269d5d7a68310f3
praveenk53 <praveenk53ngup@gmail.com>
2026-08-19 20:24:49 +0530

    Add files via upload

---
+++ 03_input_hello.py
@@ -0,0 +1,4 @@
+# Ask the user for their name and age, then print a greeting like “Hello Praveen, you are 25 years old!”
+name = input("enter your name? ")
+age = int(input("enter your age? "))
+print(f"hello {name} , you are {age} years old")
