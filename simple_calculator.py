4e500e9b5c6789c1f70e6524b2fda5718ffd3447
praveenk53 <praveenk53ngup@gmail.com>
2026-08-19 20:25:06 +0530

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,8 @@
+#Simple calculator – ask for two numbers and print their sum, difference, product, and division.
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+print(f" addition  : {a} + {b} = {a+b}")
+print(f" subtraction  : {a} - {b} = {a-b}")
+print(f" multiplication  : {a} * {b} = {a*b}")
+print(f" division  : {a} / {b} = {a/b}")
+
