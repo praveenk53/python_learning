ca3bc1f0e66b89682a8af9d7547a0a5872d97167
praveenk53 <praveenk53ngup@gmail.com>
2026-08-20 20:48:25 +0530

    Add files via upload

---
+++ 06_age_vote.py
@@ -0,0 +1,6 @@
+#Ask the user for their age. If age >= 18, print “You are eligible to vote.” Else print “You are not eligible.”
+age = int(input("enter your age: "))
+if age>=18:
+    print("you are eligible to vote ")
+else:
+    print("you are not eligible ")
