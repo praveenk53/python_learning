c7c67069bd3e48f3b92228c0ad24723e7878daac
praveenk53 <praveenk53ngup@gmail.com>
2026-08-20 20:48:45 +0530

    Add files via upload

---
+++ 07_pos_or_neg.py
@@ -0,0 +1,8 @@
+#Ask for a number and check if it is positive, negative, or zero.
+num = int(input("enter a valid number: "))
+if(num>0):
+    print("number is positive ")
+elif(num<0):
+    print("number is negative ")
+else:
+    print("number is zero ")
