3fada83f80a74dc72ebe4a2f893a8cce444ce56b
praveenk53 <praveenk53ngup@gmail.com>
2026-08-24 22:25:26 +0530

    Add files via upload

---
+++ 05_add_two_numbers.py
@@ -0,0 +1,4 @@
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter another number: "))
+sum = number_1 + number_2
+print(f"the sum = {sum}")
---
+++ 06_to_uppercase.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+name = name.upper()
+print(name)
+
+## another way to do it
+
+name2 = input("What is your name? ")
+print(name2.upper())
