23887323d2c8dd68b5cba4c72fc6c28e26e2e8e9
praveenk53 <praveenk53ngup@gmail.com>
2026-09-07 22:06:22 +0530

    Add files via upload

---
+++ 21_language_converter.py
@@ -0,0 +1,9 @@
+lang = input("enter a language : ")
+print(f"i like {lang.replace('java' , ' python')}")
+
+# sentence method
+
+sentence = input("enter sentence: ")
+print(f"{sentence.replace('python' , 'java ')}")
+
+
---
+++ 22_even_or_odd.py
@@ -0,0 +1,5 @@
+num = int(input("enter the number : "))
+if (num % 2 == 0 ):
+    print ("its even ")
+else:
+    print("its odd ")
