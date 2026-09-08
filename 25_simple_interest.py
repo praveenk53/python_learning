57fe1e2b3f3a0e882febf1a06c432a87afc12b8d
praveenk53 <praveenk53ngup@gmail.com>
2026-09-08 19:16:17 +0530

    Add files via upload

---
+++ 25_simple_interest.py
@@ -0,0 +1,4 @@
+principal = int(input("enter principle amount: "))
+rate = int(input("enter rate of interest: "))
+time = int(input("enter time in years: "))
+print(f" simple interest = {principal * rate * time / 100}")
